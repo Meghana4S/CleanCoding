da7836763e18d3ec68f9dd92d988705537cc55c2
Meghana S <120075522+Meghana4S@users.noreply.github.com>
2025-03-13 15:28:53 +0530

    Add files via upload

---
+++ DAY2/TemplateEngine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateEngine.Tests
+{
+    public class TemplateEngine
+    {
+        private String template;
+        private String value;
+        private readonly Dictionary<string, string> variables = new Dictionary<string, string>();
+
+        public string evaluate()
+        {
+            string result = template;
+            foreach (var variable in variables)
+            {
+                result = result.Replace("{" + variable.Key + "}", variable.Value);
+            }
+            return result;
+        }
+
+        public void setTemplate(string template)
+        {
+            this.template = template;
+        }
+
+        public void setVariable(string name, string value)
+        {
+            if (variables.ContainsKey(name))
+            {
+                variables[name] = value;
+            }
+            else
+            {
+                variables.Add(name, value);
+            }
+        }
+    }
+}
---
+++ DAY2/TemplateEngineTestSuite.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateEngine.Tests
+{
+    public class TemplateEngineTestSuite
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase("Theju")]
+        public void ShouldWorkForOneVariable(string value)
+        {
+            //Arrange
+            TemplateEngine templateEngine = new TemplateEngine();
+            templateEngine.setTemplate("Hello, {name}");
+            templateEngine.setVariable("name", value);
+
+            //Act
+            string result = templateEngine.evaluate();
+
+            //Assert
+            Assert.That("Hello, "+ value, Is.EqualTo(result));
+        }
+
+        [TestCase("Theju", "Meghu")]
+        public void ShouldWorkForOTwoVariable(string first, string last)
+        {
+            //Arrange
+            TemplateEngine templateEngine = new TemplateEngine();
+            templateEngine.setTemplate("Hello, {name}");
+            templateEngine.setVariable("name", first + " " + last);
+
+            //Act
+            string result = templateEngine.evaluate();
+
+            //Assert
+            Assert.That("Hello, " + first + " " + last, Is.EqualTo(result));
+        }
+
+        [TestCase("Theju", "Meghu", "Developer")]
+        public void ShouldWorkForNVariable(string first, string last, string title)
+        {
+            //Arrange
+            TemplateEngine templateEngine = new TemplateEngine();
+            templateEngine.setTemplate("Hello, {first} {last}, the {title}");
+            templateEngine.setVariable("first", first);
+            templateEngine.setVariable("last", last);
+            templateEngine.setVariable("title", title);
+
+            //Act
+            string result = templateEngine.evaluate();
+
+            //Assert
+            Assert.That("Hello, " + first + " " + last + ", the " + title, Is.EqualTo(result));
+        }
+    }
+}
+
